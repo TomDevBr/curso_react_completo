48ec1ec89fd8e2f3da1f5a849351fffa2350cb64
TomDevBr <tomasjocalheiros@gmail.com>
2024-10-09 11:11:20 -0300

    arquivos das aulas de formulario

---
+++ src/components/formularios/input/input.jsx
@@ -0,0 +1,21 @@
+const App = () => {
+  const [nome, setNome] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(nome);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="nome">Nome</label>
+      <input
+        type="text"
+        id="nome"
+        value={nome}
+        onChange={(event) => setNome(event.target.value)}
+      />
+      <button>Enviar</button>
+    </form>
+  );
+}
---
+++ src/components/formularios/radio/radio.jsx
@@ -0,0 +1,39 @@
+const App = () => {
+    const [radio, setRadio] = React.useState('');
+  
+    function handleChange({ target }) {
+      setRadio(target.value);
+    }
+  
+    return (
+      <form>
+        <label>
+          <input
+            type="radio"
+            value="notebook"
+            checked={radio === 'notebook'}
+            onChange={handleChange}
+          />
+          Notebook
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="smartphone"
+            checked={radio === 'smartphone'}
+            onChange={handleChange}
+          />
+          Smartphone
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="tablet"
+            checked={radio === 'tablet'}
+            onChange={handleChange}
+          />
+          Tablet
+        </label>
+      </form>
+    );
+  };
